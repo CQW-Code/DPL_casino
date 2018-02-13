18466aa4abfb22c35a8c41ba8edede862e85959a
Bobby Caya <bob.caya1@gmail.com>
2018-02-12 22:09:12 -0700

    adds cards

--- high_low.rb
+++ high_low.rb
@@ -22,24 +22,14 @@ class HighLow
         numeric_value2(card2)
         puts "Your second card is the #{card2.rank} of #{card2.suit}"
         case  user_input
-<<<<<<< HEAD
             when "1"
                 if card1.value < card2.value 
-=======
-            when 1
-                if @value1 < @value2  
->>>>>>> 88d5ea2c7979a27700f9ea38d352df7b9abe6eb3
                     puts "You win!"
                 else 
                     puts "You lose!"
                 end 
-<<<<<<< HEAD
             when "2"
                 if card1.value > card2.value
-=======
-            when 2
-                if @value1 > @value2 
->>>>>>> 88d5ea2c7979a27700f9ea38d352df7b9abe6eb3
                     puts "You win!"
                 else 
                     puts "You lose!"
@@ -50,31 +40,5 @@ class HighLow
     end 
      
 
-<<<<<<< HEAD
-=======
-    def numeric_value(card1)
-        faces = { 'J' => 11, 'Q' => 12, 'K' => 13, 'A'=> 1}
-        begin 
-            if Integer(card1.rank).is_a?(Numeric)
-                @value1 = card1.rank.to_i
-            end
-        rescue => e 
-            @value1 = faces[card1.rank]
-        end 
-        @value1 
-    end 
-    
-    def numeric_value2(card2)
-        faces = { 'J' => 11, 'Q' => 12, 'K' => 13, 'A'=> 1}
-        begin 
-            if Integer(card2.rank).is_a?(Numeric)
-                @value2 = card2.rank.to_i
-            end
-        rescue => e 
-            @value2 = faces[card2.rank]
-        end 
-        @value2 
-    end 
->>>>>>> 88d5ea2c7979a27700f9ea38d352df7b9abe6eb3
 end 
 HighLow.new 
